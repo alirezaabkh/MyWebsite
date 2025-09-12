2aac0e22678b0830c365213570178ba07b7cf1c1
alirezaabkh <alirezabakhtiari458@gmail.com>
2025-09-12 10:14:46 +0330

    summary 1
    create templates , static, media

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from website.views import *
+
+urlpatterns = [
+    path('', index_view),
+]
+
+
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
