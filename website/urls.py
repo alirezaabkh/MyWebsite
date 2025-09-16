7e523955dee8d34f9730c74bbd5a1086d7a084b0
alirezaabkh <alirezabakhtiari458@gmail.com>
2025-09-16 11:33:46 +0330

    create blog app
    creating blog app and connect to mysite

--- website/urls.py
+++ website/urls.py
@@ -1,17 +1,11 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', index_view),
-    path('about', about_view),
-    path('contact', contact_view)
+    path('', index_view, name='index'),
+    path('about', about_view,  name='about'),
+    path('contact', contact_view, name='contact')
 ]
-
-
-
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
