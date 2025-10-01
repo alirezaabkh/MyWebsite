d8932010a90c73b250d42d86c016b10d7f910b32
alirezaabkh <alirezabakhtiari458@gmail.com>
2025-10-01 11:44:14 +0330

    create folder of custom template tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post , Category
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,5 @@ class PostAdmin(admin.ModelAdmin):
 
 # Register your models here.
 
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
