547a1f0e6b99b08e70065ea4f58a99a7a255a647
alirezaabkh <alirezabakhtiari458@gmail.com>
2025-10-03 00:30:07 +0330

    inclusion tags for popular posts and categories

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,5 @@ def blog_single(request, pid):
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
 
-# def test(request, pid):
-#     post = get_object_or_404(Post, id=pid)
-#     context = {'post':post}
-#     return render(request, 'test.html', context)
+def test(request):
+    return render(request, 'test.html')
